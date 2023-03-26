dde50f3e8d1025a258b701490ef7b26e9531de50
LoginiStrong <sandmanlms@charter.net>
2023-03-26 16:40:59 -0700

    Updated turn system

--- Assets/Scipts/TurnSystem.cs
+++ Assets/Scipts/TurnSystem.cs
@@ -9,6 +9,7 @@ public class TurnSystem : MonoBehaviour
     private List<CharacterScript> characters;
     private CharacterScript tempCharacter;
     private GameObject temp;
+    public bool middleOfTurn;
 
 
     // Start is called before the first frame update
@@ -25,7 +26,44 @@ public class TurnSystem : MonoBehaviour
 
     void startPlaying()
     {
-        Debug.Log("HI");
+        while (characters.Count > 0)
+        {
+            for (int i = 0; i < characters.Count; i++)
+            {
+                if (characters[i].getType() == "skeleton" || characters[i].getType() == "horse")
+                {
+                    Debug.Log("Enemy");
+                    while (middleOfTurn == true)
+                    {
+                        endTurn();
+                    }
+                }
+                else
+                {
+                    Debug.Log("Player");
+                    makingMove();
+                    while (middleOfTurn == true)
+                    {
+
+                    }
+                }
+            }
+        }
+
     }
+
+
+    void makingMove()
+    {
+        Debug.Log("Choose a move");
+        middleOfTurn = true;
+    }
+
+    public void endTurn()
+    {
+        middleOfTurn = false;
+    }
+
+
 
 }
